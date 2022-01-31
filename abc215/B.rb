166cd4f62731c263e33e3c899650e341ef48d320
Naoki Oketani <okepy.naoki@gmail.com>
2022-01-31 21:54:48 +0900

    [AC] abc215_b

---
+++ abc215/B.rb
@@ -0,0 +1,7 @@
+n = gets.to_i
+k = 0
+while n > 1
+  n /= 2
+  k += 1
+end
+puts k
--- abc215/spec/B_spec.rb
+++ abc215/spec/B_spec.rb
@@ -20,4 +20,17 @@ RSpec.describe 'test' do
     expect(io.readlines.join).to eq("59\n")
   end
 
+  it 'test with "576460752303423487\n"' do
+    io = IO.popen("ruby abc215/B.rb", "w+")
+    io.puts("576460752303423487\n")
+    io.close_write
+    expect(io.readlines.join).to eq("58\n")
+  end
+
+  it 'test with "288230376151711743\n"' do
+    io = IO.popen("ruby abc215/B.rb", "w+")
+    io.puts("288230376151711743\n")
+    io.close_write
+    expect(io.readlines.join).to eq("57\n")
+  end
 end
