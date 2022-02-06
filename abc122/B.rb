baaee4c7d26f5b127b74d6d796c406cbdfe8ccf1
Naoki Oketani <okepy.naoki@gmail.com>
2022-02-06 22:42:02 +0900

    [AC] abc122_b

---
+++ abc122/B.rb
@@ -0,0 +1,12 @@
+s = gets.chomp
+longest = 0
+current = 0
+s.each_char do |c|
+  if ['A', 'C', 'G', 'T'].include?(c)
+    current += 1
+    longest = current if longest < current
+  else
+    current = 0
+  end
+end
+puts longest
