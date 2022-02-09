df3f1ddc0b068b5850d5aa04476fe266bc960cfa
Naoki Oketani <okepy.naoki@gmail.com>
2022-02-09 20:18:37 +0900

    [AC] abc095_c

--- abc095/spec/C_spec.rb
+++ abc095/spec/C_spec.rb
@@ -1,3 +1,22 @@
 RSpec.describe 'test' do
+  it 'test with "1500 2000 1600 3 2\n"' do
+    io = IO.popen("ruby abc095/C.rb", "w+")
+    io.puts("1500 2000 1600 3 2\n")
+    io.close_write
+    expect(io.readlines.join).to eq("7900\n")
+  end
 
+  it 'test with "1500 2000 1900 3 2\n"' do
+    io = IO.popen("ruby abc095/C.rb", "w+")
+    io.puts("1500 2000 1900 3 2\n")
+    io.close_write
+    expect(io.readlines.join).to eq("8500\n")
+  end
+
+  it 'test with "1500 2000 500 90000 100000\n"' do
+    io = IO.popen("ruby abc095/C.rb", "w+")
+    io.puts("1500 2000 500 90000 100000\n")
+    io.close_write
+    expect(io.readlines.join).to eq("100000000\n")
+  end
 end
