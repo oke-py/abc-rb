fe1c253d4f96fc83d5f26266eb00898039df3f22
Naoki Oketani <okepy.naoki@gmail.com>
2022-02-21 21:59:16 +0900

    [AC] abc145_c

---
+++ abc145/C.rb
@@ -0,0 +1,15 @@
+N = gets.to_i
+towns = Array.new(N) { gets.chomp.split.map { |e| e.to_i } }
+ans = 0
+(1..N).to_a.permutation(N).each do |perm|
+  distance = 0
+  prev = nil
+  perm.each do |i|
+    if !prev.nil?
+      distance += Math.sqrt((towns[i - 1][0] - prev[0]) ** 2 + (towns[i - 1][1] - prev[1]) ** 2)
+    end
+    prev = towns[i - 1]
+  end
+  ans += distance
+end
+puts (ans / (1..N).inject(1,:*)).round(10)
