9ae2280b43072be3c4fd785f8bd9ca0187b6e0d6
Naoki Oketani <okepy.naoki@gmail.com>
2022-02-22 20:22:34 +0900

    [AC] abc150_c

---
+++ abc150/C.rb
@@ -0,0 +1,9 @@
+N = gets.to_i
+pn = gets.split.join.to_i
+qn = gets.split.join.to_i
+numbers = (1..N).to_a.permutation.map do |perm|
+  perm.join.to_i
+end.sort
+a = numbers.index(pn)
+b = numbers.index(qn)
+puts a < b ? b - a : a -b
