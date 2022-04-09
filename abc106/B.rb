d5bba7a0709dffe7ac0104fcd0840b0ade195b1e
Naoki Oketani <okepy.naoki@gmail.com>
2022-04-09 12:43:31 +0900

    中級編 全探索：全列挙 2

--- abc106/B.rb
+++ abc106/B.rb
@@ -1,10 +1,12 @@
-n = gets.to_i
+# frozen_string_literal: true
+
+N = gets.to_i
 ans = 0
-1.step(n, 2) do |i|
-  d = 0
-  1.step(n, 2) do |j|
-    d += 1 if i % j == 0
+1.step(by: 2, to: N) do |i|
+  factors = 0
+  1.step(by: 2, to: i) do |j|
+    factors += 1 if (i % j).zero?
   end
-  ans += 1 if d == 8
+  ans += 1 if factors == 8
 end
 puts ans
