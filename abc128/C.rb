461fb5af46377c79c1d28b4ff24072cc74b3cc50
Naoki Oketani <okepy.naoki@gmail.com>
2022-04-21 19:11:00 +0900

    中級編 全探索：ビット全探索 11

--- abc128/C.rb
+++ abc128/C.rb
@@ -1,26 +1,33 @@
-n, m = gets.chomp.split.map(&:to_i)
-ss = []
-m.times do
-  tmp = gets.chomp.split.map(&:to_i)
-  tmp.shift
-  ss << tmp
+# frozen_string_literal: true
+
+N, M = gets.chomp.split.map(&:to_i)
+bulbs = []
+M.times do
+  s = gets.chomp.split.map(&:to_i)
+  s.shift
+  bulbs << s
 end
 
 ps = gets.chomp.split.map(&:to_i)
 
 ans = 0
-(1 << n).times do |bit|
-  valid = true
-  m.times do |i|
-    count = ss[i].count { |s| bit[s - 1] == 1 }
 
-    if count % 2 != ps[i]
-      valid = false
+(1 << N).times do |bits|
+  ok = true
+
+  M.times do |i|
+    on = 0
+    bulbs[i].each do |s|
+      on += 1 if bits[s - 1] != 0
+    end
+
+    if on % 2 != ps[i]
+      ok = false
       break
     end
   end
 
-  ans += 1 if valid
+  ans += 1 if ok
 end
 
 puts ans
